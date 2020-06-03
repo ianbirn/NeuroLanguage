40de928a6aaee956f85566c327d2609fbfe20db6
cmaclaren <cmaclarenn@gmail.com>
2020-06-03 16:36:20 -0500

    outline to test two files

--- c/main.c
+++ c/main.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+#include "gen_randomhv.h"
+#include "cos_angle.h"
+
+int main() {
+	
+    int x = 10000;
+	double u[x];
+	double v[x];
+	
+	int *ptr = genRandomHV(x);
+	
+	for (int i=0; i<x; i++) {
+		printf("%i ", ptr[i]);
+	}
+	
+	initialization(u, x);
+	initialization(v, x);
+	
+	double distance = cosAngle(u, v, x);
+	
+	printf("%g\n", distance);
+	
+	return 0;
+}
