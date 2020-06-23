192815e88bc804aead91fc9d5effc78ac8b9d422
cmaclaren <cmaclarenn@gmail.com>
2020-06-23 16:15:38 -0500

    added comments and makefile

--- c/main.c
+++ c/main.c
@@ -3,31 +3,47 @@
 #include "train.h"
 #include "computations.h"
 #include "test.h"
-
+#include <time.h>
 
 int main() {
-    int N = 4;
-    int D = 10000;
 
-    char itemMemory[27] = "abcdefghijklmnopqrstuvwxyz ";
+/**These values can be altered to best speed/accuracy ratings**/    
+    int N = 5;
+    int D = 1000;
+
+/**For timing purposes, if you wish to not time, then comment out the following and other instances**/
+    clock_t t1;
+    clock_t t2;
+    double trainTime;
+    double testTime;
+/****************************************************************************************************/
+
+/**The following are declarations and are determined based on what languages you wish to use. Currently, 
+      the default is the latin alphabet and the 22 European languages pulled in ../training_texts.  **/
+    char itemMemory[27] = "abcdefghijklmnopqrstuvwxyz ";     
     int imSize = sizeof(itemMemory);
-
 	char langLabels[][4] = {"afr", "bul", "ces", "dan", "nld", "deu", "eng", "est", "fin", "fra", "ell", "hun", "ita", "lav", "lit", "pol", "por", "ron", "slk", "slv", "spa", "swe"};
 	int length =  (sizeof langLabels)/(sizeof langLabels[0]); //size of langLabels
-    
+/******************************************************************************************************/
 
     int *imhv = (int*)malloc(imSize * D * sizeof(int));
 	int *langAM = (int*)malloc(length * D * sizeof(int));
 
+    t1 = clock();                                               //comment out to not time
 	train(N, D, itemMemory, imSize, imhv, langLabels, length);
+    t1 = clock() - t1;                                          //comment out to not time
+    trainTime  = ((double)t1)/CLOCKS_PER_SEC;                   //comment out to not time
+
+    t2 = clock();                                               //comment out to not time
 	storeLangAM(N, D, langAM, length, langLabels);
 	test(N, D, langAM, imhv, itemMemory, imSize);
+    t2 = clock() - t2;                                          //comment out to not time
+    testTime = ((double)t2)/CLOCKS_PER_SEC;                     //comment out to not time
 
+    printf("Training: %.3fs\nTesting: %.3fs\nTotal: %.3fs\n", trainTime, testTime, (trainTime+testTime));   //comment out to not time
 
     free(imhv);
 	free(langAM);
-
 	return 0;
-	
 } 
 
--- c/train.h
+++ c/train.h
@@ -1,11 +1,22 @@
 #ifndef TRAIN_H
 #define TRAIN_H
 
+/**Creates the cache directory**/
 void createCache();
+
+/**Creates a directory for new N & D values**/
 void createND(int N, int D, char cachedND[]);
+
+/**Loads in the hypervectors into character files**/
 void loadiM(int D, int imSize, char cachedND[], char itemMemory[]);
+
+/**Stores the character files' hypervectors into an array (for speed purposes)**/
 void createIMHV(int D, int imSize, char itemMemory[], char cachedND[], int *imhv);
+
+/**Builds the assistive memory for the different language files**/
 void buildLangHV(int N, int D, int length, char langLabels[][4], char cachedND[], int imSize, char itemMemory[], int *imhv);
+
+/**Runs all the above functions and checks if directories exist and have all necessary files**/
 void train(int N, int D, char itemMemory[], int imSize, int *imhv, char langLabels[][4], int length);
 
 #endif
