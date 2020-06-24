a4a6ad29e1095f9a7d50833e88cc1ac1a2049503
cmaclaren <cmaclarenn@gmail.com>
2020-06-24 09:15:30 -0500

    revised timing to prevent overflow over 2000 s

--- c/main.c
+++ c/main.c
@@ -3,17 +3,18 @@
 #include "train.h"
 #include "computations.h"
 #include "test.h"
-#include <time.h>
+#include <sys/time.h>
 
 int main() {
 /**For timing purposes, if you wish to not time, then comment out the following and other instances**/
-    clock_t t1;
+    struct timeval start, end; 
+    struct timeval start2, end2; 
     double trainTime;
     double testTime;
 /****************************************************************************************************/
 
 /**These values can be altered to best speed/accuracy ratings**/    
-    int N = 5;
+    int N = 4;
     int D = 10000;
 
 /**The following are declarations and are determined based on what languages you wish to use. Currently, 
@@ -27,16 +28,19 @@ int main() {
     int *imhv = (int*)malloc(imSize * D * sizeof(int));
 	int *langAM = (int*)malloc(length * D * sizeof(int));
 
-    t1 = clock();                                               //comment out to not time
+    gettimeofday(&start, NULL);                                            //comment out to not time
 	train(N, D, itemMemory, imSize, imhv, langLabels, length);
-    t1 = clock() - t1;                                          //comment out to not time
-    trainTime  = ((double)t1)/CLOCKS_PER_SEC;                   //comment out to not time
+    gettimeofday(&end, NULL);                                       //comment out to not time
+    
+    trainTime = (end.tv_sec - start.tv_sec) * 1e6; 
+    trainTime = (trainTime + (end.tv_usec - start.tv_usec)) * 1e-6; 
 
-    t1 = clock();                                               //comment out to not time
+    gettimeofday(&start2, NULL);                                                //comment out to not time
 	storeLangAM(N, D, langAM, length, langLabels);
 	test(N, D, langAM, imhv, itemMemory, imSize);
-    t1 = clock() - t1;                                          //comment out to not time
-    testTime = ((double)t1)/CLOCKS_PER_SEC;                     //comment out to not time
+    gettimeofday(&end2, NULL);                                           //comment out to not time
+    testTime = (end2.tv_sec - start2.tv_sec) * 1e6; 
+    testTime = (testTime + (end2.tv_usec - start2.tv_usec)) * 1e-6;                    //comment out to not time
 
     printf("Training: %.3fs\nTesting: %.3fs\nTotal: %.3fs\n", trainTime, testTime, (trainTime + testTime));   //comment out to not time
 
