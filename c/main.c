ee7fdffb1447b7a43dbf6b30a74b25aa632641c2
cmaclaren <cmaclarenn@gmail.com>
2020-06-24 13:32:36 -0500

    added more accurate timing function

--- c/main.c
+++ c/main.c
@@ -9,15 +9,14 @@
 int main() {
     srand(time(0));
 /**For timing purposes, if you wish to not time, then comment out the following and other instances**/
-    struct timeval start, end; 
-    struct timeval start2, end2; 
-    double trainTime;
-    double testTime;
+    struct timespec start1, end1;
+    struct timespec start2, end2;
+    double trainTime, testTime;
 /****************************************************************************************************/
 
 /**These values can be altered to best speed/accuracy ratings**/    
     int N = 3;
-    int D = 10000;
+    int D = 5000;
 
 /**The following are declarations and are determined based on what languages you wish to use. Currently, 
       the default is the latin alphabet and the 22 European languages pulled in ../training_texts.  **/
@@ -30,21 +29,21 @@ int main() {
     int *imhv = (int*)malloc(imSize * D * sizeof(int));
 	int *langAM = (int*)malloc(length * D * sizeof(int));
 
-    gettimeofday(&start, NULL);                                            //comment out to not time
+    clock_gettime(CLOCK_MONOTONIC, &start1);                                         //comment out to not time
 	train(N, D, itemMemory, imSize, imhv, langLabels, length);
-    gettimeofday(&end, NULL);                                       //comment out to not time
+    clock_gettime(CLOCK_MONOTONIC, &end1);                                       //comment out to not time
 
-    gettimeofday(&start2, NULL);                                                //comment out to not time
+    clock_gettime(CLOCK_MONOTONIC, &start2);                                     //comment out to not time
 	storeLangAM(N, D, langAM, length, langLabels);
 	test(N, D, langAM, imhv, itemMemory, imSize);
-    gettimeofday(&end2, NULL);                                           //comment out to not time
-    
-    trainTime = (end.tv_sec - start.tv_sec) * 1e6; 
-    trainTime = (trainTime + (end.tv_usec - start.tv_usec)) * 1e-6; 
-    testTime = (end2.tv_sec - start2.tv_sec) * 1e6; 
-    testTime = (testTime + (end2.tv_usec - start2.tv_usec)) * 1e-6;                    //comment out to not time
+    clock_gettime(CLOCK_MONOTONIC, &end2);
 
-    printf("Training: %.3fs\nTesting: %.3fs\nTotal: %.3fs\n", trainTime, testTime, (trainTime + testTime));   //comment out to not time
+    trainTime = (end1.tv_sec - start1.tv_sec) * 1e9;                          //comment out to not time
+    trainTime = (trainTime + (end1.tv_nsec - start1.tv_nsec)) * 1e-9; 
+    testTime = (end2.tv_sec - start2.tv_sec) * 1e9;                                     //comment out to not time
+    testTime = (testTime + (end2.tv_nsec - start2.tv_nsec)) * 1e-9; 
+
+    printf("Training: %.3fs\nTesting: %.3fs\nTotal: %.3fs\n", trainTime, testTime, (trainTime+testTime));                                  //comment out to not time
 
     free(imhv);
 	free(langAM);
