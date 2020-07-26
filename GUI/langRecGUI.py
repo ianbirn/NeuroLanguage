00be9f62847057c907bd99bc2196ad457ebd1c0b
cmaclaren <cmaclarenn@gmail.com>
2020-07-25 21:02:07 -0400

    moved GUI objects

---
+++ GUI/langRecGUI.py
@@ -0,0 +1,306 @@
+from tkinter import *
+from tkinter import ttk
+import ctypes
+import subprocess
+import os
+from threading import Thread
+from decimal import Decimal
+from pythonFunctions import *
+
+iM={}
+langLabels = np.array(['afr', 'bul', 'ces', 'dan', 'nld', 'deu', 'eng', 'est', 'fin', 'fra', 'ell', 'hun', 'ita', 'lav', 'lit', 'pol', 'por', 'ron', 'slk', 'slv', 'spa', 'swe'])
+langAM={}
+
+def next_button():
+    awesome.destroy()
+    continueB.destroy()
+    train = Label(text="\n**Before using the program for the first time,\n remember to train a function mode**\n")
+    train.pack()
+    color = Label(text="The program offers a color coding method based\n on how confident it is:", font='bold')
+    color.pack()
+    red = Label(text="\nRed indicates the lowest confidence\n", fg='red')
+    red.pack()
+    orange = Label(text="Orange shows that the program is pretty confident\n", fg='dark orange')
+    orange.pack()
+    green = Label(text="Green means it is almost fully certain of its guess\n", fg="green")
+    green.pack()
+    now = Label(text="If you're ready, please exit out \nand restart the program to begin!")
+    now.pack()
+    
+
+def sharedlibfunc():
+    subprocess.call("cc -fPIC -shared -o main.so main.c", shell=True)
+    createB.destroy()
+    welcome.destroy()
+    since.destroy()
+    click.destroy()
+    open('first_time.txt', 'a').close()
+    global awesome
+    awesome = Label(root, text="\n\nAwesome! Now let's learn how to use the software.\n")
+    awesome.pack()
+    global continueB
+    continueB = Button(root, text="Continue->", command=next_button)
+    continueB.pack()
+    
+
+if os.path.isfile("./first_time.txt") == False:
+    root = Tk()
+    root.title("Set up")
+    root.geometry('415x400')
+    welcome = Label(root, text="Welcome", font=("Courier", 18))
+    welcome.pack()
+    since = Label(root, text="\nSince this is your first time,\nlet's get set up.")
+    since.pack()
+    click = Label(root, text="First let's create the shared library,\nclick the button below!\n\n")
+    click.pack()
+    createB = Button(root, text="Create", command=sharedlibfunc)
+    createB.pack()
+    root.mainloop()
+    
+else:
+    prg = Tk()
+    prg.title("Language Recognition")
+
+    dirstring = os.path.abspath(os.getcwd())
+    so_file = dirstring + "/main.so"          #CONNECTION TO C CODE
+    main = ctypes.CDLL(so_file)
+
+    #import subprocess
+    #subprocess.call(["gcc", "/home/pi/Desktop/internship/main.c"])
+    #tmp = subprocess.call("./a.out")
+
+    main.program.restype = ctypes.c_double
+    main.program.argtypes = [ctypes.c_int, ctypes.c_int]
+
+    main.sentence.restype = ctypes.c_int
+    main.sentence.argtypes = [ctypes.c_int, ctypes.c_int]
+
+    main.sentencePercentage.restype = ctypes.c_double
+    main.sentencePercentage.argtypes = [ctypes.c_int, ctypes.c_int]
+
+    N = 3
+    D = 5000
+    langNum = 22
+    choice = 1
+    list1 = ["Afrikaans", "Bulgarian", "Czech", "Danish", "Dutch", "German", "English", "Estonian", "Finnish", "French", "Greek", "Hungarian", "Italian", "Latvian", "Lithuanian", "Polish", "Portuguese", "Romanian", "Slovak", "Slovenian", "Spanish", "Swedish", "error"]
+    l = IntVar()
+    r = IntVar()
+    r.set("2")
+    count = 1
+
+    def button_detect():
+        new_path = "pyFile.txt"
+        if os.path.isfile(new_path) == False:
+            print("Error pyFile.txt is missing\n")
+        entryFile = open(new_path, "w")
+        if len(e.get()) == 0:
+            ans.config(text="Please enter something first", fg="black")
+            return
+        entryFile.write(e.get())
+        entryFile.close()
+        
+        if var.get() == 'Python':
+            path = 'cachedTraining/' + str(D) + '_' + str(N)
+            if not os.path.isdir(path):
+                print('folder does not exist!')
+                exit()
+	
+            #read in the training files
+            for arr, name in readTrainingFiles(path + '/'):
+                if (name[0:2] == 'im'):
+                    newKey = os.path.splitext(name)[0][3:]
+                    iM[newKey] = arr
+                if (name[0:2] == 'la'):
+                    newKey = os.path.splitext(name)[0][3:]
+                    langAM[newKey] = arr
+	
+        #print(iM)
+        #print(langAM)
+        #testing the function
+        
+            lang = testHV(iM, langAM, N, D, langLabels, str(e.get()))
+            print(lang)
+            ans.config(text='My guess: ' + lang[0])
+        else:
+            langNum = main.sentence(N, D)
+            percentage = main.sentencePercentage(N, D)
+            percentage = round(percentage, 2)
+            if langNum == -1:
+                ans.config(text="Please train that setting first!")
+            elif langNum == -2:
+                ans.config(text="Please train a setting first!")
+            else:
+                lang = list1[langNum]
+                if percentage < 60:
+                    ans.config(text="My guess: " + lang + " " + str(percentage) + "% match", fg="red", font='bold')
+                elif percentage < 80:
+                    ans.config(text="My guess: " + lang + " " + str(percentage) + "% match", fg="dark orange", font='bold')
+                elif percentage <= 100:
+                    ans.config(text="My guess: " + lang + " " + str(percentage) + "% match", fg="green", font='bold')
+            #global survey
+            #survey = Label(prg, text="Was I correct?")
+            #survey.grid(row=3, column=0, padx=15, pady=15)
+            #global yesButton
+            #global noButton
+            #yesButton = Button(prg, text="Y", command=lambda: survey_buttons(1))
+            #yesButton.grid(row=3, column=1)
+            #noButton = Button(prg, text="N", command=lambda: survey_buttons(2))
+            #noButton.grid(row=3, column=2)
+            #global surveyReaction
+            #surveyReaction = Label(prg, text="")
+            #surveyReaction.grid(row=4, column=1)
+            
+          
+    def run_threading2():
+        for btn in buttons:
+            btn['state'] = 'disabled'
+            
+        progress_train.start(interval=10)
+        print('started training')
+        button_detect()
+        progress_train.stop()
+        print('ended training')
+        
+        for btn in buttons:
+            btn['state'] = 'normal'
+            
+    def real_detect():
+        Thread(target= run_threading2).start()
+
+    def button_clear():
+        e.delete(0, END)
+        
+    def button_train():
+        iM={}
+        langAM={}
+        if var.get() == 'Python':
+            iM, langAM = buildLanguageHV(iM, langAM, langLabels, N, D)
+	
+            #check if folder exists, create or clear it
+            path = 'cachedTraining/' + str(D) + '_' + str(N)
+            if not os.path.isdir(path):
+                os.mkdir(path)
+	
+            #write itemMemory
+            for key, value in iM.items():
+                filename = 'im_' + key + '.csv'
+                with open(path + '/' + filename, 'w') as thefile:
+                    np.savetxt(thefile, value, delimiter=',')
+
+            for key, value in langAM.items():
+                filename = 'la_' + key + '.csv'
+                with open(path + '/' + filename, 'w') as thefile:
+                    np.savetxt(thefile, value, delimiter=',')
+        else:
+            data, values = os.pipe()
+            valuestr = str(N) + " " + str(D) + "\n"
+            os.write(values, bytes(valuestr, "utf-8"));
+            os.close(values)
+            s = subprocess.check_output("gcc -O5 main.c -o main -lm;./main", stdin = data, shell=True)
+            print(s.decode("utf-8"))
+        
+        if N==5:
+            done.config(text="Done training low setting!")
+        elif N==3:
+            done.config(text="Done training medium setting!")
+        elif N==4:
+            done.config(text="Done training high setting!")
+        else:
+            done.config(text="check your code you did something wrong")
+        
+    def run_threading():
+        for btn in buttons:
+            btn['state'] = 'disabled'
+            
+        progress_train.start(interval=10)
+        print('started training')
+        button_train()
+        progress_train.stop()
+        print('ended training')
+        
+        for btn in buttons:
+            btn['state'] = 'normal'
+        
+    def real_training():
+        Thread(target=run_threading).start()
+        
+    def kill_window():
+        newWindow.destroy()
+        yesButton.destroy()
+        noButton.destroy()
+        surveyReaction.destroy()
+        survey.destroy()
+        
+    def survey_buttons(choice):
+        if choice == 1:
+            yesButton.destroy()
+            noButton.destroy()
+            surveyReaction.destroy()
+            survey.destroy()
+        elif choice == 2:
+            count = 1
+            global newWindow
+            newWindow = Toplevel(prg)
+            newWindow.title("Improvements")
+            surveyTitle = Label(newWindow, text="Please select the correct language manually")
+            surveyTitle.grid(row=0, column=0, padx=10, pady=10)
+            surveyTitle.config(font=14)
+            for name in list1:
+                if name == "error":
+                    break
+                Radiobutton(newWindow, text=name, variable=l, value=count).grid(row=count, column=0, sticky='w')
+                count = count +1
+            submit = Button(newWindow, text="Submit", command=kill_window).grid(row=25, column=0, padx=10, pady=10)
+        else:
+            surveyReaction.config(text="")
+            
+    def mode(value):
+        global N
+        global D
+        if value == 1:
+            N = 5
+            D = 1000
+        elif value == 2:
+            N = 3
+            D = 5000
+        elif value == 3:
+            N = 4
+            D = 10000
+        
+    progress_train = ttk.Progressbar(prg, orient=HORIZONTAL, length=200, mode='indeterminate')
+    progress_train.grid(row=1, column=4, padx=15, pady=15)
+        
+    Radiobutton(prg, text="Low Function", variable=r, value=1, command=lambda: mode(r.get())).grid(row=0, column=3)
+    Radiobutton(prg, text="Medium Function", variable=r, value=2, command=lambda: mode(r.get())).grid(row=1, column=3)
+    Radiobutton(prg, text="High Function", variable=r, value=3, command=lambda: mode(r.get())).grid(row=2, column=3)
+
+    writeFileB = Button(prg, text="Detect Language", command = real_detect, padx=50, pady=10)
+    writeFileB.grid(row=1, column=0)
+    writeFileB.config(font=14)
+
+    clearB = Button(prg, text="Clear", command = button_clear, padx=30, pady=10)
+    clearB.grid(row=1,column=1, columnspan=2)
+
+    trainB = Button(prg, text="Train Me!", command = real_training)
+    trainB.grid(row=0, column=4)
+    trainB.config(font=14)
+
+    ans = Label(prg, text="My guess: ", font='bold')
+    ans.grid(row=2, column=0, padx=10, pady=10)
+
+    done = Label(prg, text=" ")
+    done.grid(row=2, column=4)
+    e = Entry(prg, borderwidth=5, width=40)
+    e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+
+    options = ['C','Python']
+    var = StringVar(prg)
+    var.set(options[0])
+    optmenu = OptionMenu(prg, var, *options).grid(row=3, column=4)
+    
+    #BUTTON MATRIX
+    buttons = [writeFileB, clearB, trainB]
+
+    prg.mainloop()
+
---
+++ GUI/pythonFunctions.py
@@ -0,0 +1,139 @@
+import numpy as np
+import os
+import glob
+import fnmatch
+import time
+#will read a training file and yield the buffer, looping thru langLabels
+def readLabels(langLabels): 
+	i = 0
+	while i < langLabels.size:
+		address = 'training_texts/' + langLabels[i] + '.txt'
+		buff = []
+		for row in open(address, 'r'):
+			buff.append(row)
+		yield np.array(buff, dtype='str'), i
+		i+=1
+
+#given numpy arr, scan each letter one by one and yield it
+def getKeys(arr): 
+	i = 0
+	for line in arr:
+		for char in line:
+			yield char
+			
+			
+#this gen function will scan through the testing_files and return buffer and file name
+def readTestFiles(path): 
+	pattern = '*.txt'
+	for textFile in os.listdir(path):
+		if fnmatch.fnmatch(textFile, pattern):
+			buff=[]
+			for row in open(os.path.join(path, textFile)):
+				buff.append(row)
+			yield np.array(buff, dtype='str'), textFile
+	
+def readTrainingFiles(path): 
+	pattern = '*.csv'
+	for textFile in os.listdir(path):
+		if fnmatch.fnmatch(textFile, pattern):
+			with open(os.path.join(path, textFile)) as thefile:
+				buff = np.loadtxt(thefile, delimiter=',')
+				yield buff, textFile
+
+
+
+#this function will generate a completely random array of 1's and -1's of
+# size D
+def genRandomHV (D):
+	if D % 2:
+		print('dimension is odd!')
+	else:
+		randomIndex = np.random.permutation(D)
+		randomHV = np.zeros((1, D), dtype='int')
+		half = D/2
+		randomHV[0, np.where(randomIndex < half)] = 1
+		randomHV[0, np.where(randomIndex >= half)] = -1
+	return randomHV
+
+#given a dictionary, a Key, and D, if key exists in dictionary return value, if not
+# create a new hypervector and enter it into the dictionary as a new key and return the new
+# dictionary and new hypervector
+def lookupItemMemory(itemM, key, D):
+	if key in itemM:
+		randomHV = itemM[key]
+	else:
+		itemM[key] = genRandomHV(D)
+		randomHV = itemM[key]
+	
+	return itemM, randomHV
+
+#core function. takes hypervectors for letters N at a time and circularly shifts them down
+#and to the right. the N block is multiplied and added to the sumHV to create the languageHV
+def computeSumHV(text, itemMemory, N, D):
+	block = np.zeros((N, D), dtype='int')
+	sumHV = np.zeros((1, D), dtype='int')
+	i = 0
+	for key in getKeys(text):
+		block = np.roll(block, 1, [0,1])
+		
+		itemMemory, block[0,:] = lookupItemMemory(itemMemory, key, D)
+		
+		if i >= N:
+			nGrams = block[0, :]
+			for j in range(1, N, 1):
+				nGrams = np.multiply(nGrams, block[j, :])
+			sumHV = np.add(sumHV, nGrams)
+		
+		i+=1
+	return itemMemory, sumHV
+
+#helper function to turn a language hypervector into a binarized version
+def binarizeHV(v):
+	threshold = 0
+	vsize = v.size
+	for i in range(0,vsize):
+		if v[0,i] > threshold:
+			v[0,i] = 1
+		else:
+			v[0,i] = -1
+	return v
+
+#compute the similarity between two hypervectors
+def cosAngle(u, v):
+	cos = np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
+	return cos
+
+
+def buildLanguageHV(iM, langHV, langLabels, N, D):
+	langAM={}
+	for data, i in readLabels(langLabels):
+		iM, langHV = computeSumHV(data, iM, N, D)
+		langAM[langLabels[i]] = langHV
+		print('file read')
+		
+	print(iM)
+	print(langAM)
+	return iM, langAM
+
+#read testing files and calculate the similarities between them and the 
+#language hypervectors and output the accuracy
+def testHV(iM, langAM, N, D, langLabels, s):
+	accuracy={}
+	
+	iMn, textHV = computeSumHV(s, iM, N, D)
+	textHV = binarizeHV(textHV)
+	
+	if np.not_equal(iM, iMn):
+		print('------NEW ITEM IN TEST SENTENCE-----')
+	else:
+		maxAngle = -1
+		for label in langLabels:
+			angle = cosAngle(langAM[label], textHV[0,:])
+			accuracy[label] = angle
+		
+	
+	## sort the list
+	sortAccuracy = sorted(accuracy.items(), key=lambda kv:(kv[1], kv[0]), reverse=True)
+	print(sortAccuracy)
+	return sortAccuracy[0]
+	
